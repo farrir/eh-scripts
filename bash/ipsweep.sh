c39bf28ba35cf05c06e717fc6d582e7ec8b474a9
farrir <39937972+farrir@users.noreply.github.com>
2020-06-06 10:50:29 +0200

    Update ipsweep.sh

--- bash/ipsweep.sh
+++ bash/ipsweep.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # sends a ping w/ 1 packet to each ip address in the given X.X.X net
-# prints out all IP that are reachable 
+# prints out all IP that are reachable
 
-for ip in `seq 1 254` ; do
-    ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
-done
+if [ -n $1 ]; then
+    echo "net missing for ipsweep (format X.X.X)"
+else
+    for ip in `seq 1 254` ; do
+        ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    done
+fi
